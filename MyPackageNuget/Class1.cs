fc421ef83dddbd71209d8a3ce9467dd2649d174f
umudoff <u.amrah@gmail.com>
2021-04-14 13:36:29 +0200

    Add project files.

---
+++ MyPackageNuget/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyPackageNuget
+{
+    public class Class1
+    {
+        async Task DownloadAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            await httpClient.GetStringAsync("");
+        }
+    }
+}
